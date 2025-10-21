db9df0b87d8efdd375cf80a5cc76e323fb50ada8
JoranBergfeld <joranbergfeld@hotmail.com>
2025-10-21 09:55:53 +0200

    adds application insights

--- src/main/java/com/joranbergfeld/agentic_devops_demo/AgenticDevopsDemoApplication.java
+++ src/main/java/com/joranbergfeld/agentic_devops_demo/AgenticDevopsDemoApplication.java
@@ -3,10 +3,16 @@ package com.joranbergfeld.agentic_devops_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
 @SpringBootApplication
 public class AgenticDevopsDemoApplication {
 
 	public static void main(String[] args) {
+		String applicationInsightsConnectionString = System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING");
+		if (applicationInsightsConnectionString != null && !applicationInsightsConnectionString.isEmpty()) {
+			ApplicationInsights.attach();
+		}
 		SpringApplication.run(AgenticDevopsDemoApplication.class, args);
 	}
 
